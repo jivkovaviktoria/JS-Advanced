c658f866261dfdff9a1ed5d07b4801957c446045
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-17 13:45:01 +0200

    Solved "Math operations" from Syntax, Functions and Statements - lab

---
+++ 01.SyntaxFunctionsAndStatements-lab/05.MathOperations.js
@@ -0,0 +1,10 @@
+function MathOperations(num1, num2, operator){
+    switch (operator){
+        case '+': console.log(num1+num2); break;
+        case '-': console.log(num1-num2); break;
+        case '/': console.log(num1/num2); break
+        case '*': console.log(num1*num2); break;
+        case '%': console.log(num1%num2); break;
+        case '**': console.log(num1**num2); break;
+    }
+}
