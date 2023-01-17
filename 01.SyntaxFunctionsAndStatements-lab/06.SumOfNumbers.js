f9d1205e8e8cf2eeca5c5ccf648e52ed159b35dd
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2023-01-17 13:48:06 +0200

    Solved "Sum of numbers N...M" from Syntax, Functions and Statements - lab

---
+++ 01.SyntaxFunctionsAndStatements-lab/06.SumOfNumbers.js
@@ -0,0 +1,10 @@
+function SumOfNumbers(n, m){
+    let result = 0;
+    let num1 = Number(n);
+    let num2 = Number(m);
+
+    for(let i = num1; i <= num2; i++)
+        result += i;
+
+    console.log(result);
+}
